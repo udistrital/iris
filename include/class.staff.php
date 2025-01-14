eeed6b702a766345d3a2d23970578a1cb499cd18
ludwinggsanchez <ludwinggsanchez@gmail.com>
2025-01-14 10:30:19 -0500

    feat Múltiples administradores dependencia
    - Botón de dar acceso global no modifica el rol de las dependencias.

--- include/class.staff.php
+++ include/class.staff.php
@@ -1387,9 +1387,13 @@ implements AuthenticatedUser, EmailContact, TemplateVariable, Searchable {
             $access = array();
             if ($vars['submit'] == 'Dar Acceso Global') {
                 $depts = Dept::getDepartments();
-                foreach ($depts as $id => $_) {
+                foreach (array_diff(array_keys($depts), @$vars['dept_access']) as $id) {
                     if ($vars['dept_id'] != $id)
                         $access[] = array($id, 4, 1);
+                }
+                foreach (array_intersect(array_keys($depts), @$vars['dept_access']) as $dept_id) {
+                    $access[] = array($dept_id, $vars['dept_access_role'][$dept_id],
+                        @$vars['dept_access_alerts'][$dept_id]);
                 }
             } else if (isset($vars['dept_access'])) {
                 foreach (@$vars['dept_access'] as $dept_id) {
